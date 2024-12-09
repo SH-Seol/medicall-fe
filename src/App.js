f4e6a14e630888f42910b73c3bd4904c2ec57b10
설승환 <tjftmd7@gmail.com>
2024-12-09 22:19:39 +0900

    feat: 이미지 파일 및 app.js 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-import WelcomePage from "./components/WelcomPage";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Loading from "./components/Loading";
+import LoginPage from "./components/LoginPage";
+import LoginTerm from "./components/LoginTerm";
+import MainPage from "./components/MainPage";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-function App() {
-  return (
-    <div>
-      <WelcomePage />
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                {/* 초기 결정 라우트: 토큰 및 약관 동의 여부 확인 후 라우팅 */}
+                <Route path="/loading" element={<Loading />} />
+
+                {/* 로그인 페이지 */}
+                <Route path="/login" element={<LoginPage />} />
+
+                {/* 약관 동의 페이지 */}
+                <Route path="/login/term" element={<LoginTerm />} />
+
+                {/* 보호된 경로: 토큰 없으면 /login으로 이동 */}
+                <Route
+                    path="/main"
+                    element={
+                        <ProtectedRoute>
+                            <MainPage />
+                        </ProtectedRoute>
+                    }
+                />
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
